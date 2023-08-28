e60314b8d061966ef98bdb849022b6b938a6246a
vamsi <gvk4@hotmail.com>
2023-08-28 13:25:21 +0530

    new terraform course material from Zeal Vora

--- terraform-on-aws-ec2/03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
+++ terraform-on-aws-ec2/03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
@@ -10,7 +10,7 @@ terraform {
 }  
 # Provider Block
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-southeast-2"
 }
 
 /*
--- terraform-on-aws-ec2/03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
+++ terraform-on-aws-ec2/03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
@@ -1,7 +1,7 @@
 # Resource: EC2 Instance
-resource "aws_instance" "myec2vm" {
-  ami = "ami-0742b4e673072066f"
-  instance_type = "t3.micro"
+resource "aws_instance" "TerraformEc2" {
+  ami = "ami-0373aa64534d82bf6"
+  instance_type = "t2.micro"
   user_data = file("${path.module}/app1-install.sh")
   tags = {
     "Name" = "EC2 Demo"
