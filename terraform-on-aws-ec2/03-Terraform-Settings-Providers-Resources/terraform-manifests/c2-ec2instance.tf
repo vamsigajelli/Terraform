e0625bf511a6fbd3dfce2435bbde13c8bde65902
vamsigajelli <47843802+vamsigajelli@users.noreply.github.com>
2023-08-30 13:23:08 +0530

    Delete terraform-on-aws-ec2 directory

--- terraform-on-aws-ec2/03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
+++
@@ -1,9 +0,0 @@
-# Resource: EC2 Instance
-resource "aws_instance" "TerraformEc2" {
-  ami = "ami-0373aa64534d82bf6"
-  instance_type = "t2.micro"
-  user_data = file("${path.module}/app1-install.sh")
-  tags = {
-    "Name" = "EC2 Demo"
-  }
-}
